9bc2426cd83bf1a15a108947724df9c66dbe63e2
Arnoldas <141561525+arnoldasstrukcinskas@users.noreply.github.com>
2025-05-14 12:52:40 +0200

    Delete cmake-build-debug directory
    delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/fc-tmp/tinyxml2/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for tinyxml2")
-
-block(SCOPE_FOR VARIABLES)
-
-include("D:/C++/Programavimo praktika(zaidimas)/cmake-build-debug/CMakeFiles/fc-tmp/tinyxml2/tinyxml2-gitupdate.cmake")
-
-endblock()
--- cmake-build-debug/_deps/flac-build/microbench/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/C++/Programavimo praktika(zaidimas)/cmake-build-debug/_deps/flac-src/microbench
-# Build directory: D:/C++/Programavimo praktika(zaidimas)/cmake-build-debug/_deps/flac-build/microbench
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
